7ce34f9c6c6a3b0754d67f09534697d51a67814e
Adam Gibson <ekaggata@gmail.com>
2015-01-18 19:34:29 +0200

    encryption end intermediate commit

--- taker.py
+++ taker.py
@@ -268,6 +268,10 @@ class Taker(OrderbookWatch):
                     continue
                 self.cjtx.recv_txio(nick, utxo_list, cj_pub, change_addr)
             elif chunks[0] == 'sig':
+                #whether the signature is correct or not,
+                #we treat this as the last message for this tx from
+                #this nick, and therefore switch off encryption
+                self.end_encryption(nick)
                 sig = chunks[1]
                 self.cjtx.add_signature(sig)
 
@@ -358,7 +362,7 @@ def main():
     seed = sys.argv[1]  #btc.sha256('your brainwallet goes here')
     keyfile = sys.argv[2]
     from socket import gethostname
-    nickname = 'taker-' + btc.sha256(gethostname())[:6]
+    nickname = 'taker-' + sys.argv[2][:3] + btc.sha256(gethostname())[:6]
 
     print 'downloading wallet history'
     wallet = Wallet(seed, max_mix_depth=5)
