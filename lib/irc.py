10d3a4c7ef17d42cea03d92718dbcf44144d7a71
Belcher <belcher@email>
2015-04-19 16:14:07 +0100

    added code for possibility of maker pushing the fully-signed tx, see issue#56

--- lib/irc.py
+++ lib/irc.py
@@ -12,7 +12,7 @@ PING_INTERVAL = 40
 PING_TIMEOUT = 10
 encrypted_commands = ["auth", "ioauth", "tx", "sig"]
 plaintext_commands = ["fill", "error", "pubkey", "orderbook", "relorder",
-                      "absorder"]
+                      "absorder", "push"]
 
 
 def get_irc_text(line):
@@ -99,6 +99,10 @@ class IRCMessageChannel(MessageChannel):
         for nick in nick_list:
             self.__privmsg(nick, 'tx', txb64)
             time.sleep(1)  #HACK! really there should be rate limiting, see issue#31
+
+    def push_tx(self, nick, txhex):
+        txb64 = base64.b64encode(txhex.decode('hex'))
+        self.__privmsg(nick, 'push', txb64)
 
     #Maker callbacks
     def announce_orders(self, orderlist, nick=None):
@@ -243,6 +247,14 @@ class IRCMessageChannel(MessageChannel):
                         self.send_error(nick, 'bad base64 tx. ' + repr(e))
                     if self.on_seen_tx:
                         self.on_seen_tx(nick, txhex)
+                elif chunks[0] == 'push':
+                    b64tx = chunks[1]
+                    try:
+                        txhex = base64.b64decode(b64tx).encode('hex')
+                    except TypeError as e:
+                        self.send_error(nick, 'bad base64 tx. ' + repr(e))
+                    if self.on_push_tx:
+                        self.on_push_tx(nick, txhex)
             except CJPeerError:
                 #TODO proper error handling
                 debug('cj peer error TODO handle')
